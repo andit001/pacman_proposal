0362af7d0e6237558ad31420453752c81a7c17ba
andit001 <andreas.tomasini@gmail.com>
2022-11-05 11:06:24 +0100

    Removed unused import.

--- src/framework/Scene.java
+++ src/framework/Scene.java
@@ -2,7 +2,6 @@ package framework;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
 
 public class Scene extends ComponentBehaviour {
     private String sceneName;
